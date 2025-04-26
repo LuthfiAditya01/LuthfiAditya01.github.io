b5f31412146ed9c43310601d4562caec4349b92b
LuthfiAditya01 <luthfi2004aditya@gmail.com>
2025-04-27 00:31:08 +0700

    fix minor issues, add animation, and use prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
     extend: {
       fontFamily: {
-        'inter': ['Inter', 'sans-serif'],
-        'DynaPuff': ['DynaPuff', 'cursive'],
-        'Limelight': ['Limelight', 'cursive'],
-        'JetBrains': ['JetBrains Mono', 'monospace'],
-        'PermanentMarker': ['Permanent Marker', 'cursive'],
+        inter: ['Inter', 'sans-serif'],
+        DynaPuff: ['DynaPuff', 'cursive'],
+        Limelight: ['Limelight', 'cursive'],
+        JetBrains: ['JetBrains Mono', 'monospace'],
+        PermanentMarker: ['Permanent Marker', 'cursive'],
       },
     },
   },
-  plugins: [
-    require('tailwindcss-text-stroke'),
-  ],
-}
-
+  plugins: [require('tailwindcss-text-stroke')],
+};
